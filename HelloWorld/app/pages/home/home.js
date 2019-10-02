fedc3ab6c5e9cb221ed033098f3f7d70eebe1d13
Anil Neupane <anil.npn1996@gmail.com>
2019-10-02 13:48:04 +0200

    added extr pages, about, repos and contact us

---
+++ HelloWorld/app/pages/home/home.js
@@ -0,0 +1,21 @@
+const frameModule = require('ui/frame');
+
+
+let navigate  = function(page){
+    console.log(`Going to ${page}`);
+    frameModule.topmost().navigate(`pages/${page}/${page}`);
+}
+
+
+
+
+exports.onAboutTap = function(){
+   navigate('about');
+}
+
+exports.onContactUsTap = function(){
+navigate('contact-us');
+}
+exports.onReposTap = function(){
+    navigate('repos');
+    }
